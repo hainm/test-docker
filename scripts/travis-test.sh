a94ae5b7f2032102cb526256a7a4ea2db057bee6
Hai Nguyen <hainm.comp@gmail.com>
2016-09-02 16:27:19 -0400

    try ../..

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -4,4 +4,4 @@ git clone https://github.com/amber-md/pytraj
 cd pytraj
 git clone https://github.com/amber-md/cpptraj
 cd tests
-docker run -t my_image nosetests -vs .
+docker run -t ../../my_image nosetests -vs .
