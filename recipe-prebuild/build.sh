6bf5b0075f9e93d46ec87ac92e8dcdb56b1648b8
Hai Nguyen <hainm.comp@gmail.com>
2016-10-29 00:33:30 -0400

    fix copying lib/*

--- recipe-prebuild/build.sh
+++ recipe-prebuild/build.sh
@@ -5,5 +5,5 @@ yes | ./configure -noX11 gnu
 source amber.sh
 make install -j4
 cp $AMBERHOME/bin/* $PREFIX/bin/
-cp -rf $AMBERHOME/lib/ $PREFIX/lib/
+cp -rf $AMBERHOME/lib/* $PREFIX/lib/
 mkdir $PREFIX/dat/ && cp -rf $AMBERHOME/dat/* $PREFIX/dat/
