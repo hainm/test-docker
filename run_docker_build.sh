e99e438c851f7a27d16e9977ea8628ee70ac334b
Hai Nguyen <hainm.comp@gmail.com>
2016-10-29 20:51:00 -0400

    try centos 5

--- run_docker_build.sh
+++ run_docker_build.sh
@@ -5,8 +5,8 @@
 # tar -xf A.tar.bz2
 AMBER16=`pwd`/amber16
 # cp -rf recipe-prebuild $AMBER16/
-
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
+conda='./miniconda/bin/conda'
 
 docker info
 
@@ -14,12 +14,9 @@ config=$(cat <<CONDARC
 
 channels:
  - defaults # As we need conda-build
-
  - conda-forge
-
 conda-build:
  root-dir: /feedstock_root/build_artefacts
-
 show_channel_urls: true
 
 CONDARC
@@ -29,7 +26,7 @@ cat << EOF | docker run -i \
                         -v ${AMBER16}:/amber16 \
                         -v ${FEEDSTOCK_ROOT}:/feedstock_root \
                         -a stdin -a stdout -a stderr \
-                        condaforge/linux-anvil \
+                        centos:5.11 \
                         bash || exit $?
 
 export PYTHONUNBUFFERED=1
@@ -38,15 +35,15 @@ echo "$config" > ~/.condarc
 # A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
 conda clean --lock
 
-conda update --yes --all
-conda install --yes conda-build
-conda info
-
 yum -y install csh flex wget perl
 
 # Embarking on 1 case(s).
-    # conda build /feedstock_root/test-docker/amber16/recipe-prebuild --quiet || exit 1
     cd /amber16/
-    conda build recipe-prebuild --quiet || exit 1
-    cp `conda build --output recipe-prebuild` /feedstock_root/test-docker/
+    export ABERHOME=`pwd`
+    ./AmberTools/src/configure_python -v 3
+    $conda update --yes --all
+    $conda install --yes conda-build
+    $conda info
+    $conda build recipe-prebuild --quiet || exit 1
+    cp `$conda build --output recipe-prebuild` /feedstock_root/test-docker/
 EOF
