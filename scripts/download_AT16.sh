c9908ac19f3a145d7048b5591611c440e75e676e
Hai Nguyen <hainm.comp@gmail.com>
2016-10-29 23:47:04 -0400

    dropbox link for AT16

---
+++ scripts/download_AT16.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget https://dl.dropboxusercontent.com/u/5494759/AmberTools16.tar.bz2
--- scripts/install_git_lfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# wget https://github.com/github/git-lfs/releases/download/v1.4.4/git-lfs-linux-amd64-1.4.4.tar.gz
-# tar -xf git-lfs-linux-amd64-1.4.4.tar.gz
-# ./git-lfs-1.4.4/git-lfs pull
-
-# turn out that git-lfs has quota to download
-# use my dropbox link
-wget https://dl.dropboxusercontent.com/u/5494759/AmberTools16.tar.bz2
