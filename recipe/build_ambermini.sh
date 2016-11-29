2fb0e728f5239175ba5612b1ae6e0060828ee217
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 18:08:34 -0500

    yacc

--- recipe/build_ambermini.sh
+++ recipe/build_ambermini.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+(cd ${AMBERHOME}/AmberTools/src/byacc && $(MAKE) install )
 (cd ${AMBERHOME}/AmberTools/src/leap && make install)
 (cd ${AMBERHOME}/AmberTools/src/sqm && make install)
 (cd ${AMBERHOME}/AmberTools/src/antechamber && make install)
