4dee83dce2e3d9ff26f8bdaf8cd088fceb2e5217
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 18:40:35 -0500

    fix sqm build

--- recipe/build_ambermini.sh
+++ recipe/build_ambermini.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+(cd ${AMBERHOME}/AmberTools/src/cifparse && make install )
+(cd ${AMBERHOME}/AmberTools/src/xblas && make -j 1 lib-amb && mv libxblas-amb.a ${AMBERHOME}/lib/)
 (cd ${AMBERHOME}/AmberTools/src/byacc && make install )
 (cd ${AMBERHOME}/AmberTools/src/leap && make install)
 (cd ${AMBERHOME}/AmberTools/src/sqm && make install)
