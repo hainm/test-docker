16e708d38ca1dd5ef298c418623b353f81898471
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 16:45:16 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-source scripts/download_AmberTools.sh
-tar -xf AmberTools16.tar.bz2 && cp -rf recipe amber16/
+# source scripts/download_AmberTools.sh
+# tar -xf AmberTools16.tar.bz2 && cp -rf recipe amber16/
 sh scripts/run_docker_build.sh
--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# tar -xf A.tar.bz2
-AMBER16=`pwd`/amber16
-# cp -rf recipe $AMBER16/
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
 echo "FEEDSTOCK_ROOT" $FEEDSTOCK_ROOT
 DOCKER_IMAGE=ambermd/manylinux-extra
@@ -11,7 +8,6 @@ BZ2FILE=/root/miniconda3/conda-bld/linux-64/ambertools-*.tar.bz2
 docker info
 
 cat << EOF | docker run -i \
-                        -v ${AMBER16}:/amber16 \
                         -v ${FEEDSTOCK_ROOT}:/feedstock_root \
                         -a stdin -a stdout -a stderr \
                         $DOCKER_IMAGE \
@@ -29,7 +25,6 @@ yum -y install gcc \
                m4
 
 # Embarking on 1 case(s).
-    cd /amber16/
     source /feedstock_root/scripts/install_miniconda.sh
     conda build recipe --quiet || exit 1
     cp $BZ2FILE /feedstock_root/
