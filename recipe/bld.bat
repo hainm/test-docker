8ec9c2a59a9529694a58e00a47433f6d2f885f9a
Hai Nguyen <hainm.comp@gmail.com>
2016-11-30 15:52:15 -0500

    bash

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,7 +2,8 @@ set CC=gcc.exe
 set CXX=g++.exe 
 set FC=gfortran.exe
 
-.\configure -noX11 gnu
+bash .\configure -noX11 gnu
+make
 mingw32-make
 mingw32-make install
 xcopy /E %PREFIX%\bin %PREFIX%\Scripts\
