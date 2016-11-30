74c9eb013c948dff40cb8788724cbfa0019b275e
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 20:33:44 -0500

    make yacc first

--- recipe/build_ambermini.sh
+++ recipe/build_ambermini.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # extract from ${AMBERHOME}/AmberTools/src/Makefile
+echo ${AMBERHOME}
+(cd ${AMBERHOME}/AmberTools/src/byacc && make install )
 (cd ${AMBERHOME}/AmberTools/src/cifparse && make install )
 (cd ${AMBERHOME}/AmberTools/src/xblas && make -j 1 lib-amb && mv libxblas-amb.a ${AMBERHOME}/lib/)
-(cd ${AMBERHOME}/AmberTools/src/byacc && make install )
 (cd ${AMBERHOME}/AmberTools/src/leap && make install)
 (cd ${AMBERHOME}/AmberTools/src/sqm && make install)
 (cd ${AMBERHOME}/AmberTools/src/antechamber && make install)
