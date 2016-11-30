455ff553bdfa68d96ebc4859178133cf5702a7b7
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 23:24:25 -0500

    update recipe/patch_amberhome/copy_and_post_process_bin.py

--- recipe/patch_amberhome/copy_and_post_process_bin.py
+++ recipe/patch_amberhome/copy_and_post_process_bin.py
@@ -14,38 +14,50 @@ import subprocess
 
 os.environ['AMBERHOME'] = sys.prefix
 {exe}_exe = os.path.join(sys.prefix, 'bin', '_{exe}')
+commands = [{exe}_exe, ] + sys.argv[1:]
 try:
     subprocess.call(commands)
 except KeyboardInterrupt:
     pass
-"""
+""".strip()
 
-def post_exe_for_prefix_folder(exe_path):
+EXCLUDED_EXE = ['teLeap']
+
+def post_process_exe_for_prefix_folder(exe_path):
     # always assuming having PREFIX env
     prefix_bin = os.getenv('PREFIX', '') + '/bin/'
+    assert os.path.exists(prefix_bin)
     basename = os.path.basename(exe_path)
     subprocess.check_call(['cp', exe_path, prefix_bin + '_' +  basename])
-    with open(prefix_bin + basename, 'w') as fh:
+
+    final_exe_path = prefix_bin + basename
+    with open(final_exe_path, 'w') as fh:
         fh.write(template.format(exe=basename))
+    subprocess.check_call(['chmod', '+x', final_exe_path])
 
 def get_all_exe_paths_requiring_amberhome():
     amberhome = os.getenv('AMBERHOME', '')
+    assert amberhome
     amber_bin = amberhome + '/bin/'
 
     all_exe = []
     for exe in glob(amber_bin + '*'):
         try:
-            output = subprocess.check_output(['grep', 'AMBERHOME', exe]).decode()
+            output = subprocess.check_output('grep "AMBERHOME is not set" {}'.format(exe),
+                                             shell=True)
+            output = output.decode()
         except subprocess.CalledProcessError:
-            pass
+            output = ''
         all_exe.extend([line.split()[2]
                        for line in output.split('\n') if 'matches' in line])
-    return set(all_exe)
+    return set(exe for exe in all_exe if exe)
     
 def main():
     for exe_path in get_all_exe_paths_requiring_amberhome():
-        print(template.format(exe=os.path.basename(exe_path)))
-        # post_exe_for_prefix_folder(exe_path)
+        if not any(word in exe_path for word in EXCLUDED_EXE):
+            print('patching {}'.format(exe_path))
+            print(template.format(exe=os.path.basename(exe_path)))
+            post_process_exe_for_prefix_folder(exe_path)
 
 if __name__ == '__main__':
     main()
