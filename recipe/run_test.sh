1ca2fc1fa4cea14b01548630f9277efcd95f863c
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 23:26:24 -0500

    add run_test.sh

---
+++ recipe/run_test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+which acdoctor && acdoctor -h
+which am1bcc && am1bcc -h
+which antechamber && antechamber -h
+which atomtype && atomtype -h
+which bondtype && bondtype -h
+which charmmgen && charmmgen -h
+# which espgen && espgen -h
+which match_atomname && match_atomname -h
+# which parmcal && parmcal -h # parmcal does not have -h option
+which parmchk && parmchk -h
+which parmchk2 && parmchk2 -h
+which prepgen && prepgen -h
+# which reduce && reduce -h
+which residuegen && residuegen -h
+which respgen && respgen -h
+which sqm && sqm -h
+which tleap && tleap -h
