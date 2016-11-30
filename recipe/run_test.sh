f2d53e72672faf6365f74e728796dbfb6eb1274a
Hai Nguyen <hainm.comp@gmail.com>
2016-11-29 23:47:46 -0500

    update run test

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-which acdoctor && acdoctor -h
-which am1bcc && am1bcc -h
-which antechamber && antechamber -h
-which atomtype && atomtype -h
-which bondtype && bondtype -h
-which charmmgen && charmmgen -h
-# which espgen && espgen -h
-which match_atomname && match_atomname -h
-# which parmcal && parmcal -h # parmcal does not have -h option
-which parmchk && parmchk -h
-which parmchk2 && parmchk2 -h
-which prepgen && prepgen -h
-# which reduce && reduce -h
-which residuegen && residuegen -h
-which respgen && respgen -h
-which sqm && sqm -h
-which tleap && tleap -h
+antechamber -h
+tleap -h
+sqm -h
+acdoctor -h
+am1bcc -h
+atomtype -h
+bondtype -h
+charmmgen -h
+# espgen -h
+match_atomname -h
+# parmcal -h # parmcal does not have -h option
+parmchk -h
+parmchk2 -h
+prepgen -h
+reduce -h
+residuegen -h
+respgen -h
