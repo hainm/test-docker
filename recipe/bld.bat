df147782beb8e7aa6e088bc1de051a852d76c605
Hai Nguyen <hainm.comp@gmail.com>
2016-12-03 00:03:44 -0500

    python

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,10 +3,10 @@ set CXX=g++.exe
 set FC=gfortran.exe
 
 set AMBERHOME=`pwd`
-.\update_amber --show-applied-patches
-.\update_amber --update
-.\update_amber --show-applied-patches
-bash .\configure -noX11 gnu
+python update_amber --show-applied-patches
+python update_amber --update
+python update_amber --show-applied-patches
+python bash .\configure -noX11 gnu
 make
 mingw32-make
 mingw32-make install
