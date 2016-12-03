4b5b1cb4a68cb96f97da41f76f25738e3d4aaa50
Hai Nguyen <hainm.comp@gmail.com>
2016-12-02 23:50:27 -0500

    update bld

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -2,6 +2,10 @@ set CC=gcc.exe
 set CXX=g++.exe 
 set FC=gfortran.exe
 
+set AMBERHOME=`pwd`
+./update_amber --show-applied-patches
+./update_amber --update
+./update_amber --show-applied-patches
 bash .\configure -noX11 gnu
 make
 mingw32-make
