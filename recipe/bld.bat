82e72de632230f4183c29f0479b34faaf60c3790
Hai Nguyen <hainm.comp@gmail.com>
2016-12-02 23:54:42 -0500

    ok, I am another one saying that win command line is suck

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,9 @@ set CXX=g++.exe
 set FC=gfortran.exe
 
 set AMBERHOME=`pwd`
-./update_amber --show-applied-patches
-./update_amber --update
-./update_amber --show-applied-patches
+.\update_amber --show-applied-patches
+.\update_amber --update
+.\update_amber --show-applied-patches
 bash .\configure -noX11 gnu
 make
 mingw32-make
