56977e4c8379ce4f942272cc7ccbb49893c60fee
Hai Nguyen <hainm.comp@gmail.com>
2016-12-04 13:56:29 -0500

    upload

---
+++ scripts/upload_to_anaconda.sh
@@ -0,0 +1,17 @@
+# adapted slightly from ``mdtraj`` package
+# coveralls
+
+echo $TRAVIS_PULL_REQUEST $TRAVIS_BRANCH
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    echo "This is a pull request. No deployment will be done."; exit 0
+fi
+
+
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+    echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
+fi
+
+if [ "$TRAVIS_OS_NAME" == "osx" ] && [ '$AMBER_BUILD_TASK' == 'ambermini' ] ; then
+    anaconda -t $TRAVIS_TO_ANACONDA upload --force -u hainm $HOME/miniconda3/conda-bld/osx-64/amber*
+fi
