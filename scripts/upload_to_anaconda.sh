ff3d234da174aefd5966442aaf9af94819f98e44
Hai Nguyen <hainm.comp@gmail.com>
2016-12-04 16:13:15 -0500

    upload

--- scripts/upload_to_anaconda.sh
+++ scripts/upload_to_anaconda.sh
@@ -1,16 +1,14 @@
-# adapted slightly from ``mdtraj`` package
-# coveralls
-
 echo "pull request = " $TRAVIS_PULL_REQUEST ", branch = " $TRAVIS_BRANCH
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "This is a pull request. No deployment will be done."; exit 0
 fi
 
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+if [ "$TRAVIS_BRANCH" != "master" ]; then
     echo "No deployment on BRANCH='$TRAVIS_BRANCH'"; exit 0
 fi
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-    anaconda -t $TRAVIS_TO_ANACONDA upload --force -u hainm $HOME/miniconda3/conda-bld/osx-64/amber*
+    echo "uploading to anaconda channel"
+    anaconda -t $TRAVIS_TO_ANACONDA upload --force -u hainm $HOME/miniconda3/conda-bld/osx-64/amber*bz2 -l rc
 fi
