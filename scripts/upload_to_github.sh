870af9fb4b2a5cf1be4721a583ed9a31d8442f56
Hai Nguyen <hainm.comp@gmail.com>
2016-12-05 17:57:29 -0500

    folder

--- scripts/upload_to_github.sh
+++ scripts/upload_to_github.sh
@@ -6,8 +6,8 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     cd $MYREPO 
     git config user.name $MYUSER
     git config user.email $MYEMAIL
-    cp $HOME/miniconda*/conda-bld/*/amber*bz2 .
-    git add amber*bz2
+    cp $HOME/miniconda*/conda-bld/*/amber*bz2 at16/
+    git add at16/amber*bz2
     # cp $HOME/miniconda*/conda-bld/*/test*bz2 .
     # git add test*bz2
     git commit -m 'from travis build number: ${TRAVIS_BUILD_NUMBER}'
