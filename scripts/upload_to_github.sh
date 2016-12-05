88759ff5d633d5e5c11100fccde58e5dc4de10c3
Hai Nguyen <hainm.comp@gmail.com>
2016-12-05 17:51:17 -0500

    build number

--- scripts/upload_to_github.sh
+++ scripts/upload_to_github.sh
@@ -2,8 +2,10 @@ git clone ${MYREPO_URL}
 cd $MYREPO 
 git config user.name $MYUSER
 git config user.email $MYEMAIL
-cp $HOME/miniconda3/conda-bld/linux-64/test*bz2 .
+# cp $HOME/miniconda*/conda-bld/*/amber*bz2 .
+# git add amber*bz2
+cp $HOME/miniconda*/conda-bld/*/test*bz2 .
 git add test*bz2
-git commit -m 'test'
-git remote add production https://${GITHUB_TOKEN}@github.com/$MYUSER/$MYREPO
-git push production master --force
+git commit -m 'from travis build number: ${TRAVIS_BUILD_NUMBER}'
+git remote add production https://${GITHUB_TOKEN}@github.com/$MYUSER/$MYREPO >& log
+git push production master --force >& log
