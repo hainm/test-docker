bb8374ef8f83db89de53fe11f01730f00628b7fd
Hai Nguyen <hainm.comp@gmail.com>
2016-12-05 00:14:47 -0500

    build different branch

--- scripts/run_docker_build.sh
+++ scripts/run_docker_build.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+echo "CIRCLE_BRANCH", $CIRCLE_BRANCH
+
+buildfull=`python -c "import os;  print(os.getenv('CIRCLE_BRANCH').startswith('circleci_'))"`
+echo 'buildfull = ' $buildfull
+
+if [ "$buildfull" == "True" ]; then
+    AMBER_BUILD_TASK='ambertools'
+    pyversion=`python -c "import os; env=os.getenv('CIRCLE_BRANCH'); print(env.strip('circleci_'))"`
+    echo "pyversion = " $pyversion
+else
+    AMBER_BUILD_TASK='ambermini'
+fi
+
+echo "AMBER_BUILD_TASK = " $AMBER_BUILD_TASK
+
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
 echo "FEEDSTOCK_ROOT" $FEEDSTOCK_ROOT
 DOCKER_IMAGE=ambermd/amber-build-box
@@ -14,11 +29,17 @@ cat << EOF | docker run -i \
                         $DOCKER_IMAGE \
                         bash || exit $?
 
-# Embarking on 1 case(s).
     export PATH=/root/miniconda3/bin:\$PATH
     conda update --all --yes
-    conda build /feedstock_root/recipe --py 2.7 --quiet || exit 1
-    conda build /feedstock_root/recipe --py 3.4 --quiet || exit 1
-    conda build /feedstock_root/recipe --py 3.5 --quiet || exit 1
+    export AMBER_BUILD_TASK=${AMBER_BUILD_TASK}
+    if [ "\${AMBER_BUILD_TASK}" == 'ambermini' ]; then
+        # build in a single containter
+        conda build /feedstock_root/recipe --py 2.7 --quiet || exit 1
+        conda build /feedstock_root/recipe --py 3.4 --quiet || exit 1
+        conda build /feedstock_root/recipe --py 3.5 --quiet || exit 1
+    else
+        # build whole ambertools, for a single python version
+        # should build each ambertools python version on each branch
+        conda build /feedstock_root/recipe --py $pyversion --quiet || exit 1
     cp $BZ2FILE /feedstock_root/
 EOF
