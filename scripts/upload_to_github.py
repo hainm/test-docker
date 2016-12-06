5bd2782941b5522785acf01baed8682a7e1fcfea
Hai Nguyen <hainm.comp@gmail.com>
2016-12-05 20:16:10 -0500

    fix copy

--- scripts/upload_to_github.py
+++ scripts/upload_to_github.py
@@ -10,20 +10,30 @@ circleci = os.getenv('CIRCLECI', '')
 if travis_os_name:
     subprocess.check_call('source scripts/check_deployment.sh', shell=True)
 
-commands = """
+commands_init = """
     git clone ${MYREPO_URL}
     cd $MYREPO 
     git config user.name $MYUSER
     git config user.email $MYEMAIL
-    cp $HOME/miniconda*/conda-bld/*/amber*bz2 at16/
+""".strip().split('\n')
+
+command_copy = 'echo'
+# ovewrite command_copy
+if travis_os_name:
+    command_copy = 'cp $HOME/miniconda*/conda-bld/*/amber*bz2 at16/'
+if circleci:
+    command_copy = 'cp $CIRCLE_ARTIFACTS/amber-build/amber*bz2 at16/'
+
+commands_add_and_push = """
     git add at16/amber*bz2
-    # cp $HOME/miniconda*/conda-bld/*/test*bz2 .
-    # git add test*bz2
     git commit -m 'push to github'
     git remote add production https://${GITHUB_TOKEN}@github.com/$MYUSER/$MYREPO >& log
     git push production master --force >& log
 """.strip().split('\n')
 
 if travis_os_name == 'osx' or circleci:
-    for command in commands:
+    for command in commands_init:
+        subprocess.check_call(command, shell=True)
+    subprocess.check_call(command_copy, shell=True)
+    for command in commands_add_and_push:
         subprocess.check_call(command, shell=True)
