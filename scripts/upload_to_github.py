3da02d2f5fbae75761e102a72e9d90741649f78d
Hai Nguyen <hainm.comp@gmail.com>
2016-12-05 23:52:18 -0500

    commit messgae

--- scripts/upload_to_github.py
+++ scripts/upload_to_github.py
@@ -14,6 +14,8 @@ if circle_branch in ['circleci_py27', 'circleci_py34', 'circleci_py35']:
 else:
     commit_message = subprocess.check_output('git log --format=%B |head -1', shell=True).decode()
 
+print('commit_message', commit_message)
+
 if not commit_message.startswith("UPLOAD"):
     print("not require to upload. Exit")
     print('Tip: use git commmit -m "UPLOAD: [your_message]" to upload')
