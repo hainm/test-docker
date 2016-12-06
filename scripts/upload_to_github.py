3ac5f4d4ddf8b60ccfb4095ef46aaf0f387433bd
Hai Nguyen <hainm.comp@gmail.com>
2016-12-06 15:31:57 -0500

    try

--- scripts/upload_to_github.py
+++ scripts/upload_to_github.py
@@ -4,11 +4,11 @@ import os
 import sys
 import subprocess
 
-travis = os.getenv('TRAVIS')
+travis = os.getenv('TRAVIS', '')
 travis_os_name = os.getenv('TRAVIS_OS_NAME', '')
 travis_branch = os.getenv('TRAVIS_BRANCH', '')
-travis_pull_request = os.getenv('TRAVIS_PULL_REQUEST')
-circleci_pull_request = os.getenv('CI_PULL_REQUEST')
+travis_pull_request = os.getenv('TRAVIS_PULL_REQUEST', '')
+circleci_pull_request = os.getenv('CI_PULL_REQUEST', '')
 circleci = os.getenv('CIRCLECI', '')
 circle_branch = os.getenv('CIRCLE_BRANCH', '')
 
@@ -26,9 +26,6 @@ if not commit_message.startswith("UPLOAD"):
     print('Tip: use git commmit -m "UPLOAD: [your_message]" to upload')
     sys.exit(0)
 
-if travis_pull_request != 'false':
-    print("This is a pull request. No deployment will be done")
-    sys.exit(0)
 if circleci_pull_request:
     print("This is a pull request. No deployment will be done")
     sys.exit(0)
@@ -36,6 +33,9 @@ if circleci_pull_request:
 if travis:
     if travis_branch != 'master':
         print("No deployment on {} branch".format(travis_branch))
+        sys.exit(0)
+    if travis_pull_request != 'false':
+        print("This is a pull request. No deployment will be done")
         sys.exit(0)
 
 commands_init = """
