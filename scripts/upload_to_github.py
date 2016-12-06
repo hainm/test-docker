193b803dda2d25f517c4c4cf2598e3094672ad71
Hai Nguyen <hainm.comp@gmail.com>
2016-12-05 23:01:37 -0500

    only one script

--- scripts/upload_to_github.py
+++ scripts/upload_to_github.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
 import os
+import sys
 import subprocess
 
-# TODO: python
+travis = os.getenv('TRAVIS')
 travis_os_name = os.getenv('TRAVIS_OS_NAME', '')
 circleci = os.getenv('CIRCLECI', '')
 
-if travis_os_name:
-    subprocess.check_call('source scripts/check_deployment.sh', shell=True)
+if os.getenv("TRAVIS_PULL_REQUEST") == 'false' or os.getenv('CI_PULL_REQUEST'):
+    print("This is a pull request. No deployment will be done")
+    sys.exit(0)
+
+if travis:
+    if os.getenv("TRAVIS_BRANCH") != "master":
+        print("No deployment on BRANCH='$TRAVIS_BRANCH'")
+        sys.exit(0)
 
 commands_init = """
     git clone ${MYREPO_URL}
@@ -16,18 +23,17 @@ commands_init = """
     cd $MYREPO 
     git config user.name $MYUSER
     git config user.email $MYEMAIL
-    ls .
 """.strip().split('\n')
 
 command_copy = 'echo'
 # ovewrite command_copy
 if travis_os_name:
-    command_copy = 'cp $HOME/miniconda*/conda-bld/*/amber*bz2 at16/'
+    command_copy = 'cp $HOME/miniconda*/conda-bld/*/amber*bz2 at16/travis/'
 if circleci:
-    command_copy = 'cp $CIRCLE_ARTIFACTS/amber-build/amber*bz2 at16/'
+    command_copy = 'cp $CIRCLE_ARTIFACTS/amber-build/amber*bz2 at16/circleci/'
 
 commands_add_and_push = """
-    git add at16/amber*bz2
+    git add at16/*
     git commit -m 'push to github'
     git remote add production https://${GITHUB_TOKEN}@github.com/$MYUSER/$MYREPO
     git push production master --force
