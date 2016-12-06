21ffe5dcee25dc761c3f74ad8affb65c485f609e
Hai Nguyen <hainm.comp@gmail.com>
2016-12-06 00:01:00 -0500

    UPLOAD: test

--- scripts/update_circle_branch.py
+++ scripts/update_circle_branch.py
@@ -17,7 +17,8 @@ for branch in ['circleci_27', 'circleci_34', 'circleci_35']:
     if branch not in all_branches:
         subprocess.check_call(['git', 'branch', branch])
     subprocess.check_call(['git', 'checkout', branch])
-    subprocess.check_call(['git', 'merge', 'master', '--no-edit'])
+    subprocess.check_call(['git', 'merge', 'master', '--squash'])
+    subprocess.check_call(['git', 'commit', '-m', '"UPLOAD: merge master"'])
     if push:
         subprocess.check_call(['git', 'push', 'origin', branch])
 
