f6842c08b9173147f5589476f1382ca5cfd5e86e
Hai Nguyen <hainm.comp@gmail.com>
2016-12-07 13:34:27 -0500

    UPLOAD: fix bash

--- scripts/circleci_build.py
+++ scripts/circleci_build.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from subprocess import check_call, check_output
+from subprocess import call, check_output
 
 commit_message = check_output('git log --format=%B |head -1', shell=True).decode()
 
@@ -8,5 +8,5 @@ if commit_message.startswith('SKIP BUILD'):
     print(commit_message)
     sys.exit(0)
 else:
-    command = "source scripts/install_miniconda.sh && source build.sh"
-    check_call(command, shell=True)
+    command = "bash scripts/install_miniconda.sh && bash build.sh"
+    call(command, shell=True)
