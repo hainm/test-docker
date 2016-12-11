3bd698dc96b1a9fd3078270f0a31f922b2b1c7b1
Hai Nguyen <hainm.comp@gmail.com>
2016-12-11 01:30:14 -0500

    make install

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,8 +6,8 @@ set AMBERHOME=%cd%
 python update_amber --show-applied-patches
 python update_amber --update
 python update_amber --show-applied-patches
-echo n | bash .\configure -noX11 gnu
-mingw32-make
-mingw32-make install
+echo n | bash .\configure -noX11 --with-python=C:\Miniconda3\python.exe gnu
+make
+make install
 xcopy /E %PREFIX%\bin %PREFIX%\Scripts\
 if errorlevel 1 exit 1
