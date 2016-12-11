a44e1d9715c98a176dcd59bbc97ceee959067a2e
Hai Nguyen <hainm.comp@gmail.com>
2016-12-11 01:40:35 -0500

    python

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ set AMBERHOME=%cd%
 python update_amber --show-applied-patches
 python update_amber --update
 python update_amber --show-applied-patches
-echo n | bash .\configure -noX11 --with-python=C:\Miniconda3\python.exe gnu
+echo n | bash .\configure -noX11 --with-python C:\Miniconda3\envs\py27\python.exe gnu
 make
 make install
 xcopy /E %PREFIX%\bin %PREFIX%\Scripts\
