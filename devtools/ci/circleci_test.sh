aa00edd40d70099b6c20262eb502defa6f0def64
HN <nhai.qn@gmail.com>
2017-03-30 23:35:03 -0400

    oops, update test

--- devtools/ci/circleci_test.sh
+++ devtools/ci/circleci_test.sh
@@ -3,10 +3,11 @@
 version=16
 function circleci_test(){
     # install conda
-    bash $HOME/ambertools-test/amber$version/AmberTools/src/configure_python --prefix $HOME
+    WORK=ambertools-binary-build
+    bash $HOME/$WORK/amber$version/AmberTools/src/configure_python --prefix $HOME
     export PATH=$HOME/miniconda/bin:$PATH
     for tarfile in `ls $HOME/TMP/amber-conda-bld/linux-64/ambertools-*.tar.bz2`; do
-        python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/test_multiple_pythons.py $tarfile
+        python $HOME/$WORK/amber$version/AmberTools/src/conda_tools/test_multiple_pythons.py $tarfile
     done
 }
 
