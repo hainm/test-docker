4e551cb94de6eb072b176f01ef563267acb55e59
HN <nhai.qn@gmail.com>
2017-09-24 23:23:16 -0400

    minor

--- conda_tools/download_binary_at_dev.py
+++ conda_tools/download_binary_at_dev.py
@@ -4,13 +4,17 @@ import requests
 def download(fn):
     url = 'https://github.com/hainm/ambertools-dev-binary/blob/master/{chunk}?raw=true'
 
-    prefix = 'tmp_osx_ATa'
+    prefix = 'tmp_osx-64_ATa'
     with open(fn, 'wb') as fh:
         for ext in 'abc':
             final_url = url.format(chunk=prefix + ext)
             print("Processing %s " % final_url)
             x = requests.get(final_url)
-            fh.write(x.content)
+            if x.ok:
+                fh.write(x.content)
+            else:
+                print("Can not download %s" % final_url)
+                return
     print("Done. Please check %s" % fn)
 
 
