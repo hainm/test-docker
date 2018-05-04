339a13debe4ed38103a728e7807022e8bb02f4b3
HN <nhai.qn@gmail.com>
2018-05-04 17:59:11 -0400

    more

--- conda_tools/fix_rpath_osx.py
+++ conda_tools/fix_rpath_osx.py
@@ -4,6 +4,7 @@ import subprocess
 import itertools
 import argparse
 
+DRY_RUN = False
 
 def is_object_file(fn):
     return os.path.isfile(fn) and 'is not an object file' not in subprocess.check_output([
@@ -21,7 +22,11 @@ def add_loader_path(fn, prefix, sub):
     relpath = os.path.relpath(prefix, os.path.abspath(os.path.dirname(fn)))
     loader_path = '@loader_path/{}/{}'.format(relpath, sub)
     if not loader_path in subprocess.check_output(['otool', '-l', fn]).decode():
-        easy_check_output(['install_name_tool', '-add_rpath', loader_path, fn])
+        cmd = ['install_name_tool', '-add_rpath', loader_path, fn]
+        if DRY_RUN:
+            print(cmd)
+        else:
+            easy_check_output(cmd)
     else:
         print("{} is already in {}".format(loader_path, fn))
 
@@ -30,9 +35,12 @@ def add_id(fn):
     """Change absolute path to @rpath/{basename}
     """
     basename = os.path.basename(fn)
-    subprocess.check_call(
-        ['install_name_tool', '-id',
-         '@rpath/%s' % basename, fn])
+    cmd = ['install_name_tool', '-id',
+         '@rpath/%s' % basename, fn]
+    if DRY_RUN:
+        print(cmd)
+    else:
+        subprocess.check_call(cmd)
 
 
 def get_file_object_from_prefix(pkg_name):
@@ -54,10 +62,14 @@ def fix_linking_libs(fn, lib_paths, prefix=None):
             lib for lib in lib_paths if lib.startswith(prefix)]
     for lib_path in lib_paths:
         basename = os.path.basename(lib_path)
-        subprocess.check_call([
+        cmd = [
             'install_name_tool', '-change', lib_path,
             '@rpath/{}'.format(basename), fn
-        ])
+        ]
+        if DRY_RUN:
+            print(cmd)
+        else:
+            subprocess.check_call(cmd)
 
 
 def get_dylibs(fn):
@@ -73,11 +85,17 @@ def handle_gfortran_libs(pkg_name):
 
 
 def main(args=None):
+    global DRY_RUN
     parser = argparse.ArgumentParser(description="Fix rpath and loader_path for files in "
         "path/{bin,lib} folders")
     parser.add_argument('path')
     parser.add_argument('--prefix', help='prefix got from running otool -L')
+    parser.add_argument('-d', '--dry-run', action='store_true')
     opt = parser.parse_args(args)
+
+    if opt.dry_run:
+        DRY_RUN = True
+
     if opt.prefix is None:
         opt.prefix = os.path.abspath(opt.path)
 
