d16bf7ce74604ee4aaf850d1b67af97755cbfcc8
HN <nhai.qn@gmail.com>
2019-04-05 22:36:56 -0400

    more

--- build_all_simplified.sh
+++ build_all_simplified.sh
@@ -11,14 +11,16 @@ notest='--no-test'
 # notest=''
 
 # Build full ambertools with python 2.7
-# conda build $recipes_dir/conda-recipe --py 2.7 $notest
+# Do not change python version here since the subsequent steps will use
+# the tarfile from python 2.
+conda build $recipes_dir/conda-recipe --py 2.7 $notest
 
 # Use tarfile from above build to build only python components in AmberTools
 # with multiple python versions
-# conda build $recipes_dir/conda-multi-python --py 3.4 $notest
-# conda build $recipes_dir/conda-multi-python --py 3.5 $notest
-# conda build $recipes_dir/conda-multi-python --py 3.6 $notest
-# conda build $recipes_dir/conda-multi-python --py 3.7 $notest
+conda build $recipes_dir/conda-multi-python --py 3.4 $notest
+conda build $recipes_dir/conda-multi-python --py 3.5 $notest
+conda build $recipes_dir/conda-multi-python --py 3.6 $notest
+conda build $recipes_dir/conda-multi-python --py 3.7 $notest
 
 # Combine all tarfiles from previous builds to create a single tarfile
 # with all python versions + non-python components
