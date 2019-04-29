139aa973631ed1274897a6eca08d2e5248ab3743
amberbot.2017 <amberbot.2017@gmail.com>
2019-04-28 23:12:08 -0400

    using -static on linux

--- conda_tools/utils.py
+++ conda_tools/utils.py
@@ -52,7 +52,7 @@ def run_configure():
     if sys.platform.startswith('darwin'):
         sh('./configure --with-python python -macAccelerate clang')
     else:
-        sh('./configure --with-python python -norism gnu')
+        sh('./configure --with-python python -norism -static gnu')
 
 
 def make_install(ncpus=4):
